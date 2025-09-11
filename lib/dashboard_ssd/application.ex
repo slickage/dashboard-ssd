a8cd67eca88abb6b16d7e6741a3c7b2d49744bc8
Anthony Kinsey <anthony@slickage.com>
2025-09-11 11:04:56 -1000

    feat: scaffold Phoenix 1.7.14 app in root (T001)

---
+++ lib/dashboard_ssd/application.ex
@@ -0,0 +1,36 @@
+defmodule DashboardSSD.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      DashboardSSDWeb.Telemetry,
+      DashboardSSD.Repo,
+      {DNSCluster, query: Application.get_env(:dashboard_ssd, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: DashboardSSD.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: DashboardSSD.Finch},
+      # Start a worker by calling: DashboardSSD.Worker.start_link(arg)
+      # {DashboardSSD.Worker, arg},
+      # Start to serve requests, typically the last entry
+      DashboardSSDWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: DashboardSSD.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    DashboardSSDWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
