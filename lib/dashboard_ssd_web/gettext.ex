7102c62b35edd30e7a9bcaec18f52a2ea93509fb
Anthony Kinsey <anthony@slickage.com>
2025-09-12 08:28:50 -1000

    chore(gettext): migrate to Gettext.Backend and use Gettext backend in modules; fix warnings-as-errors on PRs

--- lib/dashboard_ssd_web/gettext.ex
+++ lib/dashboard_ssd_web/gettext.ex
@@ -1,24 +1,12 @@
 defmodule DashboardSSDWeb.Gettext do
   @moduledoc """
-  A module providing Internationalization with a gettext-based API.
+  Gettext backend for internationalization.
 
-  By using [Gettext](https://hexdocs.pm/gettext),
-  your module gains a set of macros for translations, for example:
+  To use translation macros in a module, do:
 
-      import DashboardSSDWeb.Gettext
+      use Gettext, backend: DashboardSSDWeb.Gettext
 
-      # Simple translation
-      gettext("Here is the string to translate")
-
-      # Plural translation
-      ngettext("Here is the string to translate",
-               "Here are the strings to translate",
-               3)
-
-      # Domain-based translation
-      dgettext("errors", "Here is the error message to translate")
-
-  See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
+  See the Gettext docs for details.
   """
-  use Gettext, otp_app: :dashboard_ssd
+  use Gettext.Backend, otp_app: :dashboard_ssd
 end
