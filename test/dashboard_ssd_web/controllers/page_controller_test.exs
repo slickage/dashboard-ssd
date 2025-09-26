aefa6c4c1c6e57c1f6c54c7ccdca011782dbdaca
Anthony Kinsey <anthony@slickage.com>
2025-09-26 08:41:52 -1000

    test(coverage): replace external HTTP checks

--- test/dashboard_ssd_web/controllers/page_controller_test.exs
+++ test/dashboard_ssd_web/controllers/page_controller_test.exs
@@ -1,8 +1,24 @@
 defmodule DashboardSSDWeb.PageControllerTest do
-  use DashboardSSDWeb.ConnCase
+  use DashboardSSDWeb.ConnCase, async: true
 
-  test "GET / redirects anonymous users to auth", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert redirected_to(conn) == "/auth/google?redirect_to=/"
+  import Phoenix.Controller
+
+  alias DashboardSSDWeb.PageController
+  alias DashboardSSDWeb.PageHTML
+
+  test "home renders the static home page" do
+    conn =
+      build_conn()
+      |> Plug.Conn.fetch_query_params()
+      |> Map.put(:params, %{"_format" => "html"})
+      |> Plug.Conn.assign(:flash, %{})
+      |> put_view(PageHTML)
+      |> put_layout(false)
+      |> put_root_layout(false)
+
+    conn = PageController.home(conn, %{})
+
+    assert conn.status == 200
+    assert conn.resp_body =~ "Guides &amp; Docs"
   end
 end
