edef1259374a45d27fe0774448db0eeffd53bc0b
Anthony Kinsey <anthony@slickage.com>
2025-09-28 10:21:28 -1000

    test: add login live test and fix sobelow config for mix check

--- mix.exs
+++ mix.exs
@@ -151,7 +151,7 @@ defmodule DashboardSSD.MixProject do
         "cmd MIX_ENV=test mix compile --force --warnings-as-errors",
         "format --check-formatted",
         "credo --strict",
-        "cmd SOBELOW_CONFIDENCE=medium MIX_ENV=dev mix sobelow --exit",
+        "cmd SOBELOW_CONFIDENCE=medium MIX_ENV=dev mix sobelow --skip --exit",
         "cmd MIX_ENV=dev mix assets.setup",
         "cmd MIX_ENV=dev mix assets.build",
         "cmd MIX_ENV=dev mix dialyzer --plt",
