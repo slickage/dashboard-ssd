5e0be20dd6fde215ae74e67bc2277ee27a0d4ab0
boka <boka@slickage.com>
2025-10-22 12:52:46 -1000

    fix(config/prod): don't force ssl
    this can cause infinite redirects if we're already behind reverse proxy
    and the proxy doesn’t forward the `X-Forwarded-Proto: https` header

--- config/prod.exs
+++ config/prod.exs
@@ -6,8 +6,10 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dashboard_ssd, DashboardSSDWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true]
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+# don't redirect ssl - we'll use reverse proxy
+# force_ssl: [hsts: true]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: DashboardSSD.Finch
